c127132c2b7530a51c5c22cd7c1e126520052e9f
1101320 <a1101320@11ST1101320MN001.local>
2020-01-23 14:04:31 +0900

    code, docs: complete ch03

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -27,6 +27,6 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/hello')
     def hello():
-        return 'Hello, World!'
+        return 'Hello, World!!!!'
 
     return app
