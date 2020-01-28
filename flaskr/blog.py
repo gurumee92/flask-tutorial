99af15e96f6c86a73cf5e36af3acbc3f5ead54fe
gurumee <gurumee92@gmail.com>
2020-01-28 13:55:53 +0900

    code: Create blog/index.html

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -7,3 +7,10 @@ from flaskr.db import get_db
 
 bp = Blueprint('blog', __name__)
 
+@bp.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username FROM post p JOIN user u ON p.author_id=u.id ORDER BY created DESC'
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
