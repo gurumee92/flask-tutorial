ac604d47f5eff8365972034489841bd6d5ae519c
1101320 <a1101320@11ST1101320MN001.local>
2020-01-28 09:28:49 +0900

    docs: ch04 finish

--- flaskr/db.py
+++ flaskr/db.py
@@ -39,3 +39,8 @@ def init_db_command():
     """Clear the existing data and create new tables."""
     init_db()
     click.echo('Initialized the database.')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
