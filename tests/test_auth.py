9edb45e3d2ef88575b702eeae00aa4d19c3c173c
gurumee <gurumee92@gmail.com>
2020-01-29 14:29:27 +0900

    create test_auth

---
+++ tests/test_auth.py
@@ -0,0 +1,28 @@
+import pytest
+from flask import g, session
+from flaskr.db import get_db
+
+def test_register(client, app):
+    assert client.get('/auth/register').status_code == 200
+    response = client.post(
+        '/auth/register', data={'username': 'a', 'password': 'a'}
+    )
+    assert 'http://localhost/auth/login' == response.headers['Location']
+
+    with app.app_context():
+        assert get_db().execute(
+            "select * from user where username='a'"
+        ).fetchone() is not None
+
+
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+    ('', '', b'Username is required.'),
+    ('a', '', b'Password is required.'),
+    ('test', 'test', b'already registered'),
+))
+def test_register_validate_input(client, username, password, message):
+    response = client.post(
+        '/auth/register',
+        data={'username': username, 'password': password}
+    )
+    assert message in response.data
